ea0ea223b672035b56506a24d35a2f2eed9af3fe
thatsmeforyou-star <thatsmeforyou@gmail.com>
2026-05-07 11:50:02 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str.length()).append("#").append(str);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> result = new ArrayList<>();
+        if (str == null || str.length() == 0) {
+            return result;
+        }
+        int i = 0;
+
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') {
+                j++;
+            }
+            int length = Integer.parseInt(str.substring(i, j));
+            i = j + 1;
+            result.add(str.substring(i, i + length));
+            i += length;
+        }
+        return result;
+    }
+}
